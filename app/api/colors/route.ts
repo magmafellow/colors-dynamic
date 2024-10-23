dead890cb8763d6207e4f93ab7324403c0fc245d
magmafellow <magmafellow@gmail.com>
2024-10-23 23:14:39 +0300

    jarkie

--- app/api/colors/route.ts
+++ app/api/colors/route.ts
@@ -1,10 +1,18 @@
 export async function GET() {
   // Наши цвета, которые мы получаем через запрос
   const colors = {
-    primaryColorBg: '#fcba03',
-    primaryColorBgHover: '#6e530a',
-    primaryColor: '#144d17',
-    primaryColorHover: '#eee',
+    v1: {
+      primaryColorBg: '#fcba03',
+      primaryColorBgHover: '#6e530a',
+      primaryColor: '#144d17',
+      primaryColorHover: '#eee',
+    },
+    v2: {
+      primaryColorBg: '#9cffa2',
+      primaryColorBgHover: '#79d17f',
+      primaryColor: '#22a32b',
+      primaryColorHover: '#1b6620',
+    }
   }
 
   console.log('was activated')
--- app/ui/get-colors.tsx
+++ app/ui/get-colors.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react"
 
 export default function GetColors() {
+  const type = 'v1'
+  
   useEffect(() => {
     async function Settle() {
       const data = await fetch('/api/colors')
@@ -10,10 +12,10 @@ export default function GetColors() {
       console.log(res)
       const root = document.querySelector(':root') as any
 
-      root?.style.setProperty('--primaryColorBg', res.data.primaryColorBg)
-      root?.style.setProperty('--primaryColorBgHover', res.data.primaryColorBgHover)
-      root?.style.setProperty('--primaryColor', res.data.primaryColor)
-      root?.style.setProperty('--primaryColorHover', res.data.primaryColorHover)
+      root?.style.setProperty('--primaryColorBg', res.data[type].primaryColorBg)
+      root?.style.setProperty('--primaryColorBgHover', res.data[type].primaryColorBgHover)
+      root?.style.setProperty('--primaryColor', res.data[type].primaryColor)
+      root?.style.setProperty('--primaryColorHover', res.data[type].primaryColorHover)
     }
     Settle()
   }, [])
