ee1b2479dc0e8cd0453969df8bf38d2a1e6b35d4
magmafellow <138253744+magmafellow@users.noreply.github.com>
2024-10-23 19:58:17 +0300

    use of unstable_noStore in action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server'
 
+import { unstable_noStore } from "next/cache"
+
 export async function GetMannequinTimePlaceholder() {
+  unstable_noStore()
   await new Promise((resolve, reject) => {
     setTimeout(() => resolve(1), 3000)
   })
