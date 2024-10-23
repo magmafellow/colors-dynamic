2b2c8abd4689850b270e336194364d8bfa3901df
magmafellow <138253744+magmafellow@users.noreply.github.com>
2024-10-23 19:56:18 +0300

    disabling errors: no-explicit-any, no-unused-vars

--- app/ui/card-skeleton.tsx
+++ app/ui/card-skeleton.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function CardSkeleton() {
   return (
     <div className="primary-color w-full card flex flex-col py-4 px-7 rounded-lg border border-purple-400 h-[200px]">
       <h3 className="font-bold mb-2 rounded-md w-[75px] h-[1em] bg-slate-700 text-2xl"></h3>
